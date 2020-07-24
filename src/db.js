3d5080f3ca68c0ae630c7320194c5c392f62f127
tykim7882 <49688495+tykim7882@users.noreply.github.com>
2020-07-24 10:55:53 +0900

    heroku test

--- src/db.js
+++ src/db.js
@@ -2,10 +2,19 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
+
+// mongoose.connect(process.env.MONGO_URL, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+// });
 mongoose.connection
   .once("open", function () {
     console.log("Conection has been made!");
